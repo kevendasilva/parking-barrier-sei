6b94f882e1b4e639cae33ff511ef2422ca003a7f
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-06-23 09:46:42 -0300

    update: load configs

--- barrier/utils/load_configs.py
+++ barrier/utils/load_configs.py
@@ -8,11 +8,17 @@ def load_configs(root_dir):
     with open(config_file_path) as config_file:
       configs = json.load(config_file)
       actuator_pin = configs["actuator"]["pin"]
+      warning_led_pin = configs["leds"]["warning"]
+      success_led_pin = configs["leds"]["success"]
 
-      if actuator_pin:
+      if actuator_pin and warning_led_pin and success_led_pin:
         return {
           "actuator": {
             "pin": actuator_pin
+          },
+          "leds": {
+            "warning": warning_led_pin,
+            "success": success_led_pin
           }
         }
 
